0abd3d96e4aee5d7d121f62b6a6311a8bbf15524
Reeja0261 <reeja.maharjan023@apexcollege.edu.np>
2025-12-28 17:52:31 +0545

    Remove all Assignment1 because it went separately

--- ass2.py
+++
@@ -1,9 +0,0 @@
-# write a program that asks for 5 numbers and print the largest number
-
-def largest(arr_):   #underscore gara variable bhayo
-    max_number = arr_[0]
-    for a in arr_[1:]:
-        if a > max_number:
-            max_number = a
-        return max_number
-largest([-1,-2,-7,-5,-5])
